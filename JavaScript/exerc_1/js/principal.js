8e688278f95dc0a1fad7796c89d42eb81bb8176e
LincolnAndzza <raposaasilvaa@gmail.com>
2021-05-24 23:48:36 -0300

    Desenvolvimento Web

---
+++ JavaScript/exerc_1/js/principal.js
@@ -0,0 +1,21 @@
+function clicou() {
+	document.getElementById("agradecimento").innerHTML = "Meu Obrigado";
+	//window.open("https://www.tesla.com/");
+	//window.location.href = "https://www.tesla.com/models";
+}
+
+function trocar () {
+	document.getElementById("mousemove").innerHTML = "Obrigado por Passar";
+}
+
+function voltar () {
+	document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function trocar2 (elemento) {
+	elemento.innerHTML = "Obrigado por Passar !!!";
+}
+
+function voltar2 () {
+	elemento.innerHTML = "Passe o mouse aqui";
+}
